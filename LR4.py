b28d6c8974ddbed02adfc84a456ad10132ccc574
kuzm2ch <71035116+kuzm2ch@users.noreply.github.com>
2024-06-02 17:16:22 +0300

    Add files via upload

---
+++ LR4.py
@@ -0,0 +1,156 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+from pprint import pprint
+import pandas as pd
+from gym.envs.toy_text.taxi import TaxiEnv
+
+
+def print_full(x):
+    pd.set_option('display.max_rows', len(x))
+    print(x)
+    pd.reset_option('display.max_rows')
+
+class PolicyIterationAgent:
+    '''
+    Класс, эмулирующий работу агента
+    '''
+    def __init__(self, env):
+        self.env = env
+        # Пространство состояний
+        self.observation_dim = 500
+        # Массив действий в соответствии с документацией
+        # https://www.gymlibrary.dev/environments/toy_text/frozen_lake/
+        self.actions_variants = np.array([0,1,2,3,4,5])
+        # Задание стратегии (политики)
+        # Карта 4х4 и 4 возможных действия 
+        self.policy_probs = np.full((self.observation_dim, len(self.actions_variants)), 0.16666666)
+        # Начальные значения для v(s)
+        self.state_values = np.zeros(shape=(self.observation_dim))
+        # Начальные значения параметров
+        self.maxNumberOfIterations = 1000
+        self.theta=1e-6
+        self.gamma=0.99
+
+
+    def print_policy(self):
+        '''
+        Вывод матриц стратегии
+        '''
+        
+        if self.policy_probs[0][0] != 0.16666666:
+            #np.set_printoptions(threshold=np.inf)
+            x = TaxiEnv()
+            pos = {0:'R', 1:'G',2:'Y', 3:'B', 4:'T'}
+            print('''
++---------+
+|R: | : :G|
+| : | : : |
+| : : : : |
+| | : | : |
+|Y| : |B: |
++---------+
+                  ''')
+            print('состояние: x,y,пассажир,назначение')
+            print('Стратегия:')
+            for i in range(len(self.policy_probs)):
+                t_x,t_y,passeng,dest = x.decode(i)
+                print((t_x,t_y,pos[passeng],pos[dest]), self.policy_probs[i])
+            #np.set_printoptions(threshold=False)
+        else: 
+            print('Стратегия:')
+            pprint(self.policy_probs)
+
+
+    def policy_evaluation(self):
+        '''
+        Оценивание стратегии 
+        '''
+        # Предыдущее значение функции ценности
+        valueFunctionVector = self.state_values
+        for iterations in range(self.maxNumberOfIterations):
+            # Новое значение функции ценности
+            valueFunctionVectorNextIteration=np.zeros(shape=(self.observation_dim))
+            # Цикл по состояниям
+            for state in range(self.observation_dim):
+                # Вероятности действий
+                action_probabilities = self.policy_probs[state]
+                # Цикл по действиям
+                outerSum=0
+                for action, prob in enumerate(action_probabilities):
+                    innerSum=0
+                    # Цикл по вероятностям действий
+                    for probability, next_state, reward, isTerminalState in self.env.P[state][action]:
+                        innerSum=innerSum+probability*(reward+self.gamma*self.state_values[next_state])
+                    outerSum=outerSum+self.policy_probs[state][action]*innerSum
+                valueFunctionVectorNextIteration[state]=outerSum
+            if(np.max(np.abs(valueFunctionVectorNextIteration-valueFunctionVector))<self.theta):
+                # Проверка сходимости алгоритма
+                valueFunctionVector=valueFunctionVectorNextIteration
+                break
+            valueFunctionVector=valueFunctionVectorNextIteration
+        return valueFunctionVector               
+
+
+    def policy_improvement(self):
+        '''
+        Улучшение стратегии 
+        '''
+        qvaluesMatrix=np.zeros((self.observation_dim, len(self.actions_variants)))
+        improvedPolicy=np.zeros((self.observation_dim, len(self.actions_variants)))
+        # Цикл по состояниям
+        for state in range(self.observation_dim):
+            for action in range(len(self.actions_variants)):
+                for probability, next_state, reward, isTerminalState in self.env.P[state][action]:
+                    qvaluesMatrix[state,action]=qvaluesMatrix[state,action]+probability*(reward+self.gamma*self.state_values[next_state])
+                
+            # Находим лучшие индексы
+            bestActionIndex=np.where(qvaluesMatrix[state,:]==np.max(qvaluesMatrix[state,:]))
+            # Обновление стратегии
+            improvedPolicy[state,bestActionIndex]=1/np.size(bestActionIndex)
+        return improvedPolicy    
+
+
+    def policy_iteration(self, cnt):
+        '''
+        Основная реализация алгоритма
+        '''
+        policy_stable = False
+        for i in range(1, cnt+1):
+            self.state_values = self.policy_evaluation()
+            self.policy_probs = self.policy_improvement()
+        print(f'Алгоритм выполнился за {i} шагов.')
+
+
+def play_agent(agent):
+    env2 = gym.make('Taxi-v3',render_mode='human')
+    state = env2.reset()[0]
+    done = False
+    while not done:
+        p = agent.policy_probs[state]
+        if isinstance(p, np.ndarray):
+            action = np.random.choice(len(agent.actions_variants), p=p)
+        else:
+            action = p
+        next_state, reward, terminated, truncated, _ = env2.step(action)
+        env2.render()
+        state = next_state
+        if terminated or truncated:
+            done = True
+
+
+def main():
+    # Создание среды
+    env = gym.make('Taxi-v3')
+    env.reset()
+    # Обучение агента
+    agent = PolicyIterationAgent(env)
+    agent.print_policy()
+    agent.policy_iteration(1000)
+    agent.print_policy()
+    # Проигрывание сцены для обученного агента
+    play_agent(agent)
+
+
+if __name__ == '__main__':
+    main()
